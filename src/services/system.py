94de0f123acfb23e0e0f2f3f417dc0f1048eef0b
connerytran <connery.tran@gmail.com>
2026-06-28 14:16:48 -0400

    Fixes #7 COM bug

--- src/services/system.py
+++ src/services/system.py
@@ -3,21 +3,18 @@ from ctypes import cast, POINTER # python stdlib for interacting with C lvl code
 from comtypes import CLSCTX_ALL # COM (component object model) controls windows audio
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
+_speakers = AudioUtilities.GetSpeakers()        # aquires the default audio device (speakers) on the system
+_audio_interface = _speakers._dev.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)  # activates volume control interface on that device using COM interface iid
+_volume = cast(_audio_interface, POINTER(IAudioEndpointVolume))                     # Activate returns a generic COM object, so we must cast to IAudioEndpointVolume to access volume control methods
+
 def set_volume(volume: int) -> dict:
     """Set the system volume on the PC.
     Args:
       volume: The volume level to set (0-100)
     """
 
-    try:
-        devices = AudioUtilities.GetSpeakers()
-
-        # activates volume control interface on that device using COM interface iid
-        interface = devices._dev.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-
-        # Activate returns a generic COM object, so we must cast to IAudioEndpointVolume to access volume control methods
-        vol = cast(interface, POINTER(IAudioEndpointVolume))
-        vol.SetMasterVolumeLevelScalar(volume / 100, None)
+    try: 
+        _volume.SetMasterVolumeLevelScalar(volume / 100, None)
         return {"status": "success", "message": f"System volume set to {volume}%."}
     except Exception as e:
         return {"status": "error", "message": f"Failed to set system volume: {str(e)}"}
@@ -27,10 +24,7 @@ def set_volume(volume: int) -> dict:
 def get_volume() -> dict:
     """Get the current system volume on the PC."""
     try:
-        devices = AudioUtilities.GetSpeakers()
-        interface = devices._dev.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-        vol = cast(interface, POINTER(IAudioEndpointVolume))
-        current_volume = int(vol.GetMasterVolumeLevelScalar() * 100)
+        current_volume = int(_volume.GetMasterVolumeLevelScalar() * 100)
         return {"status": "success", "volume": current_volume, "message": f"Current system volume is {current_volume}%."}
     except Exception as e:
         return {"status": "error", "message": f"Failed to get system volume: {str(e)}"}
